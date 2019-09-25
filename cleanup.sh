9b7c20709e8edb35fe84a2cf949c67897d8d666a
Monica Hart <monica.hart@vml.com>
2019-09-25 13:35:01 -0500

    adding code to make sure all the Kubernetes nodes and deployment pods are deleted

--- cleanup.sh
+++ cleanup.sh
@@ -9,3 +9,7 @@ log() {
 log "Deleting virtual machines"
 export PATH=~/.wks/bin:$PATH
 sudo env "PATH=$PATH" footloose delete
+
+log "Deleting Kubernetes nodes and pods"
+kubectl get no --all-namespaces -o json | kubectl delete -f -
+kubectl get po --all-namespaces -o json | kubectl delete -f -
